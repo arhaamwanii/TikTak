d6b2150b66ee676b8b249480ef46741177eb0643
arhaamwanii <arhamwani765@gmail.com>
2024-02-26 16:45:46 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,18 @@
 import { useState } from "react"
-import { Gameboard } from "./Components/Gameboard"
-import { Player } from "./Components/Player"
-import { Log } from "./Components/Log"
-
+import { Player } from "./Components/Player";
 
 
 function App() { 
-  
-    const [gameTurns , setGameTurns]= useState([])  //array to manage the gamedata
-    const [activePlayer , setActivePlayer] = useState("X") //setting the initial player to X
-
-  function handleSelectSquare(rowIndex , colIndex){
-    setActivePlayer((current) => current === 'X' ? 'O' : 'X' ) // swithcing  the value between X and Y 
-    setGameTurns(prevTurns => {
-
-      let currentPlayer = "X";
-
-      if(prevTurns.length> 0 && prevTurns[0].player === "X"){
-        currentPlayer = "O"
-      }
-      //taking the ;ook at only the latest playe 
-
-      const updatedTurns = [{square : {row: rowIndex , col : colIndex } , player: currentPlayer} , ...prevTurns];  //updating in a imutable way - object is turned - which player clicked on what score row = row inedez 
-      return updatedTurns;
-      //store an object --- array full of objects
-      //update state before managing the state 
-            
-    });
-  }
-
-
 
   return (
     <main>
-      <div id="game-container">
-        <ol id="players" className="highlight-player">
-          <Player intialName={"prop one"} symbol="X" isActive={activePlayer === 'X' }/>
-          <Player intialName={"prop two"} symbol="O" isActive={activePlayer === 'O'}/>
-       </ol>
-      <Gameboard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} turns={gameTurns}/>
-      </div>
-      <Log />
+        <div id="game-container">
+        <ol id="players">  
+          <Player/>
+          <Player/>
+        </ol>
+
+        </div>
     </main>
 
   )
@@ -48,35 +20,7 @@ function App() {
 
 export default App
 
-//
-//
 
-//state that manages the info about the progress of our game up,
-//onject we are adding we can deg
-
-//game state is updated dynamically to 
-
-//on the basis of which player is playing, we need to  CHANGE THE COLOR of the player heading(player.jsx) 
-// also based on the same value we need to we need to place the value "X" or "O" in the game board 
-
-//this code is an internet banger 
-
-//this value i.e which player is playing is found in player.jsx 
-
-
-// giving both of them acess to this value -- so we lift the state up(acesstor which has acess to both )
-
-// need to knho my perecets
-
-//tyically on edata to be managed by in a single state is considered ppractise calss
-
-// putting info in the gameboaed
-//gameboard contaienr already controsl these elements buy they no 
-//one state per ingfo'
-
-//quick comment stuff
-//quick comment stuff
-//the quick brown fox jumped right over the lazy dog the quick brown fox jumped right over the lazy dog the quick brown fox jumped right over the lazy dgog
 
 
 
--- src/Components/Player.jsx
+++ src/Components/Player.jsx
@@ -1,62 +1,15 @@
 import React from 'react'
-import { useState } from 'react'
 
-export const Player = ({intialName , symbol , isActive}) => { // recieveing the inf about which player is active
-
-  const [isEditting , setIsEditting ] = useState(false)
-  const [playerName  , setPlayerName ] = useState(intialName)
-
-let edittingCheck = <span className="player-name">{intialName}</span>
-
-function handleEditClick(){
-  setIsEditting((e) => !e )
-
-}
-
-function handleChange(event){
-  console.log(event)
-  setPlayerName(event.target.value);
-}
-
-let showField = <span className="player-name">{playerName}</span>
-
-if (isEditting){
-  showField = <input type="text" required value={playerName}  onChange={handleChange} autoFocus onFocus={e => e.currentTarget.select() }/>
-}
-
-
+export const Player = () => {
   return (
-  
-      <li className={isActive ? "active" : undefined}>
-                 <span>
-                  {showField}
-                  <span className="player-symbol">{symbol} </span>
-                </span>
-                <button onClick={handleEditClick} >{isEditting ? "Save" : "Edit"}</button>
+    <div>
+        <li>
+            <span className="player">
+              <span className="player-name">Player oen</span>
+              <span className="player-symbol">Symbol</span>  
+            </span>  
         </li>
-     )
+    </div>
+
+    )
 }
-
-
-
-//feeding this then back into the elements this in that case --- makes the detault value also haha
-//based on the value 
-
-//this is used to make react alter the value of the state handle click
-//to get the text on the
-// each instance of a component reuse it nothing else changes, in the other use case  - most powerful super comlex stuff is going to be editted
-//name should be what is the function of the component
-//props is the main thing that interconnects all of this
-//when the buttin is clicked the name input filed should be replaced by an real input fiels where we could enter in our name and stuff we
-//when the edit button is cliked the the value of a variable should be altered and based on that the ui should be updated such that the input field jumps into existance and and the input should be taked 
-//doing such that the edit button says save and also the main but
-
-//ternary expression can change the value
-
-//prepopulating the input field  with the current player name
-
-//the ternary ! will flip the value in case of what is before
-
-//react is funndamentally -- does not exicute commands inmmediately it
-
-
