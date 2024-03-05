fadcc23a5f83ea681d452c3080d71c88f69f9de9
arhaamwanii <arhamwani765@gmail.com>
2024-03-05 15:29:54 +0530

    .

--- src/Components/GameBoard.jsx
+++ src/Components/GameBoard.jsx
@@ -26,3 +26,6 @@ export const GameBoard = () => {
     </ol>
   )
 }
+
+
+//simple tic tak toe game was ment to be finished by like 3 months ago lol not even that it was ment to be finished by like 5 months ago and here i am after all this time -- well here is the thing others might think i am working hard and stuff  you know the reality of that it is all messed up havent out in a complete honest day of work since months -- stream is a pain  in the ass in the scence it takes a lot of time(but tought me a lot so it was worth it) - so basically till some time ago i was half assing two things now that i have decided startups is that what i want to do i have to get over myself and start ACTING -- action produces information -- we certainly lack  info the -- now it is make my parents satisfied with the studied that enough and go all in on the startup side thats the thinng two more days damn
